c720dc5e36c395e5c9f011bc3c41dc984be9f4ae
Pataar <pietering1@gmail.com>
2018-05-25 14:39:39 +0200

    Update dotfiles to match latest configuration

--- macos/install.sh
+++ macos/install.sh
@@ -4,7 +4,7 @@ echo "Setting up your Mac..."
 
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # Update Homebrew recipes
@@ -21,13 +21,22 @@ if test $(which zsh); then
 fi
 
 # Install Composer
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
+wget https://raw.githubusercontent.com/composer/getcomposer.org/master/web/installer -O - -q | php -- --quiet
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+cp ./zsh/.zshrc ~/.zshrc
+
+source ~/.zshrc
+
 xcode-select --install
 
+if test $(which rbenv); then
+  rbenv install 2.3.0
+  rbenv global 2.3.0
+fi
+
 if test $(which npm); then
-  npm install -g cordova bower ionic depcheck
+  npm install -g cordova ionic
+  npm set registry https://gitlab.interactivestudios.nl:4873
 fi
