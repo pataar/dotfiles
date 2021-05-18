0f7b5f4a5f4f3e121271adf85de8d3f5c22f37e5
pataar <pietering1@gmail.com>
2021-05-18 10:04:01 +0200

    Add update script

---
+++ update.sh
@@ -0,0 +1,33 @@
+#!/bin/zsh
+
+COLORED='\033[0;32m'
+NC='\033[0m'
+
+echo "\033[0;36m| dotfiles | @pataar"
+echo "${COLORED}Updating the Mac of $(whoami)...${NC}"
+echo "\n";
+
+cd ~/.zsh-snap && git pull 2>/dev/null && cd -
+
+source ~/.zshrc
+
+git pull 2>/dev/null
+
+echo "${COLORED}Updating znap stuff...${NC}"
+znap status
+znap pull
+
+echo "${COLORED}Updating .zshrc stuff...${NC}"
+cp ./git/.gitconfig ~/
+cp ./zsh/.zshrc ~/
+cp ./zsh/.p10k.zsh ~/
+cp -r ./bin/ /usr/local/bin/
+
+## Install NPM dependencies and the correct registry
+if test $(which mkcert); then
+  echo "${COLORED}Updating mkcert stuff...${NC}"
+  mkcert -install 2>/dev/null
+fi
+
+echo "\033[0;36m| Done updating. | "
+echo "\n";
