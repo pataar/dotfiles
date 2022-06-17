a902513c6a7def4bcb5c11f3db2ed6aeb2ec6b14
pataar <pietering1@gmail.com>
2022-06-17 10:05:56 +0200

    improve install script

--- install.sh
+++ install.sh
@@ -15,18 +15,31 @@ if [ "$(uname)" == "Darwin" ]; then
   # Check for Homebrew and install if we don't have it
   if test ! $(which brew); then
     echo "${COLORED}Installing brew...${NC}"
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+
+  # Handle Mac platforms
+  CPU=$(uname -p)
+  if [[ "$CPU" == "arm" ]]; then
+      export PATH="/opt/homebrew/bin:$PATH"
+      export EDITOR=/opt/homebrew/bin/nano
+      alias nano=/opt/homebrew/bin/nano
+      alias oldbrew=/usr/local/bin/brew
+  else
+      export PATH="/usr/local/bin:$PATH"
+      export EDITOR=/usr/local/bin/nano
+      alias nano=/usr/local/bin/nano
   fi
 
   # Update Homebrew recipes
   brew update
 
+  brew install zsh
+
   # Install all our dependencies with bundle (See Brewfile)
   brew tap homebrew/bundle
   echo "${COLORED}Updating brew bundle...${NC}"
   brew bundle --file=./macos/Brewfile
-
-  cp -r ./bin/ /usr/local/bin/
 
   open ./iterm/monokai-remastered.itermcolors
 
@@ -40,27 +53,4 @@ if test $(which zsh); then
   chsh -s $(which zsh)
 fi
 
-echo "${COLORED}Installing .zshrc stuff...${NC}"
-
-if [ "$(whoami)" == "pieter" ] ||  [ "$(whoami)" == "pataar" ]; then
-  cp ./git/.gitconfig ~/
-fi
-
-cp ./zsh/.zshrc ~/
-cp ./zsh/.p10k.zsh ~/
-
-export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:~/bin:~/.composer/vendor/bin"
-
-## Install mkcert and the root certificate
-if test $(which mkcert); then
-  echo "${COLORED}Installing mkcert stuff...${NC}"
-  mkcert -install
-fi
-
-## Install NPM dependencies and the correct registry
-if test $(which volta); then
-  echo "${COLORED}Installing volta stuff...${NC}"
-  volta install node@lts yarn
-fi
-
-echo "\033[0;36m| Done installing. | "
+./install_2.sh
---
+++ install_2.sh
@@ -0,0 +1,30 @@
+#!/bin/zsh
+
+COLORED='\033[0;32m'
+NC='\033[0m'
+
+echo "${COLORED}Installing .zshrc stuff...${NC}"
+
+if [ "$(whoami)" == "pieter" ] ||  [ "$(whoami)" == "pataar" ]; then
+  cp ./git/.gitconfig ~/
+fi
+
+cp ./zsh/.zshrc ~/
+cp ./zsh/.p10k.zsh ~/
+
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:~/bin:~/.composer/vendor/bin"
+
+## Install mkcert and the root certificate
+if test $(which mkcert); then
+  echo "${COLORED}Installing mkcert stuff...${NC}"
+  mkcert -install
+fi
+
+## Install NPM dependencies and the correct registry
+if test $(which volta); then
+  echo "${COLORED}Installing volta stuff...${NC}"
+  volta install node@lts yarn
+fi
+
+echo "\033[0;36m| Done installing. | "
+
